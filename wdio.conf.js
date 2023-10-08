8917eae51792a49f16805739889241dae29c935e
LeDaiVuong <ledaivuong1990@gmail.com>
2023-10-08 22:17:36 +0700

    try headless chrome

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,7 +51,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu']
+        }
     }],
 
     //
